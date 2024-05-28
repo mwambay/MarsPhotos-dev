ad5d73b07332b51f6000ab90c1d4c942cab61fac
Mwambay <21mk406@esisalama.org>
2024-05-28 09:46:38 +0300

    Ajout des interfaces Api

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,4 +1,29 @@
 package com.example.marsphotos.network
 
-class MarsApiService {
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+import retrofit2.converter.scalars.ScalarsConverterFactory
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos(): String
 }
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+}
+
+@GET("photos")
+suspend fun getPhotos() {}
