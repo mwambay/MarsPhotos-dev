bbefb01182ab9be07d13d18d0d112b3cf7645fec
Mwambay <21mk406@esisalama.org>
2024-05-28 10:05:07 +0300

    Autorisation pour acceder a internet

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,29 +1,49 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.marsphotos.network
 
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-import retrofit2.converter.scalars.ScalarsConverterFactory
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
 
-
+/**
+ * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+ */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
-
+/**
+ * Retrofit service object for creating api calls
+ */
 interface MarsApiService {
     @GET("photos")
-    fun getPhotos(): String
+    suspend fun getPhotos(): String
 }
 
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
 object MarsApi {
-    val retrofitService : MarsApiService by lazy {
+    val retrofitService: MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java)
     }
 }
-
-@GET("photos")
-suspend fun getPhotos() {}
